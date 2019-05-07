8511c94981e8ecf267a36cd4454e682ba6e8fe0f
kdilot <kdilot@gmail.com>
2019-05-07 11:53:51 +0900

    index

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './main.scss';
+import { Main } from 'views';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Main />
+      </div>
+    );
+  }
+}
+
+export default App;
